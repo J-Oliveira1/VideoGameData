37bc8ec2eb761c71783a7017d99bbb532c85ebd2
unknown <j.poliveira90@outlook.com>
2023-02-28 15:39:00 -0500

    Completed SearchBar function to display VideoGames and data.

--- video-game-data-frontend/src/Components/SearchBar/SearchBar.jsx
+++ video-game-data-frontend/src/Components/SearchBar/SearchBar.jsx
@@ -1,31 +1,32 @@
-import React, { useState } from 'react';
-import SearchResults from '../SearchResults/SearchResults';
-
+import React, { useState } from "react";
+import SearchResults from "../SearchResults/SearchResults";
 
 const SearchBar = ({ videoGames, filteredGames, setFilteredGames }) => {
+  const [searchName, setSearchName] = useState("");
 
-    const [searchName, setSearchName] = useState('');
+  function handleSearch(event) {
+    event.preventDefault();
+    const filteredGames = videoGames.filter((game) =>
+      game.name.includes(searchName)
+    );
+    setFilteredGames(filteredGames);
+    console.log(filteredGames)
+    setSearchName("")
+  }
 
+  return (
+    <div>
+      <form onSubmit={handleSearch}>
+        <input
+          type="search"
+          value={searchName}
+          onChange={(event) => setSearchName(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <SearchResults filteredGames={filteredGames} />
+    </div>
+  );
+};
 
-    function handleSearch(event){
-        event.preventDefault();
-        setFilteredGames(videoGames.filter(game => game.name.includes(searchName)));
-        displayFilteredGames();
-    }
-
-    function displayFilteredGames(){
-        console.log(filteredGames)
-        return(
-            <SearchResults filteredGames={filteredGames}/>
-        )
-    }
-
-    return ( 
-        <form onSubmit={handleSearch}>
-            <input type='search' value={searchName} onChange={(event) => setSearchName(event.target.value)}/>
-            <button type='submit'>Search</button>
-        </form>
-     );
-}
- 
 export default SearchBar;
--- video-game-data-frontend/src/Components/SearchResults/SearchResults.jsx
+++ video-game-data-frontend/src/Components/SearchResults/SearchResults.jsx
@@ -1,34 +1,36 @@
-import React from 'react';
+import React from "react";
 
-const SearchResults = ({filteredResults}) => {
-            <table className='table'>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Game Rank</th>
-                        <th>Name</th>
-                        <th>Platform</th>
-                        <th>Year</th>
-                        <th>Genre</th>
-                        <th>Publisher</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredResults.map((result) => {
-                        return (
-                            <tr>
-                                <td>{result.id}</td>
-                                <td>{result.game_rank}</td>
-                                <td>{result.name}</td>
-                                <td>{result.platform}</td>
-                                <td>{result.year}</td>
-                                <td>{result.genre}</td>
-                                <td>{result.publisher}</td>
-                            </tr>
-                        )
-                    })}
-                </tbody>
-            </table>
-    }
- 
+const SearchResults = ({ filteredGames }) => {
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Game Rank</th>
+          <th>Name</th>
+          <th>Platform</th>
+          <th>Year</th>
+          <th>Genre</th>
+          <th>Publisher</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredGames.map((game) => {
+          return (
+            <tr key={game.id}>
+              <td>{game.id}</td>
+              <td>{game.game_rank}</td>
+              <td>{game.name}</td>
+              <td>{game.platform}</td>
+              <td>{game.year}</td>
+              <td>{game.genre}</td>
+              <td>{game.publisher}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
 export default SearchResults;
